a6dd4fc188f86d0e38e3a9d05344dddcf284e45c
Luis Henrique Aguiar <luishenrique.aguiar@outlook.com>
2024-11-16 18:58:42 -0300

    Criação da função de conversão da temperatura e do texto do spinner

--- app/src/main/java/br/edu/ifsp/dmo/conversordetemperatura/view/MainActivity.kt
+++ app/src/main/java/br/edu/ifsp/dmo/conversordetemperatura/view/MainActivity.kt
@@ -1,12 +1,15 @@
 package br.edu.ifsp.dmo.conversordetemperatura.view
 
 import android.os.Bundle
+import android.widget.Spinner
 import androidx.appcompat.app.AppCompatActivity
 import br.edu.ifsp.dmo.conversordetemperatura.adapters.SpinnerItemAdapter
 import br.edu.ifsp.dmo.conversordetemperatura.databinding.ActivityMainBinding
+import br.edu.ifsp.dmo.conversordetemperatura.model.TemperatureConverter
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    private lateinit var strategy: TemperatureConverter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,4 +27,20 @@ class MainActivity : AppCompatActivity() {
         spinnerInitialTemperature.adapter = SpinnerItemAdapter(this, dataset)
         spinnerTargetTemperature.adapter = SpinnerItemAdapter(this, dataset)
     }
+
+    private fun setOnClickListener() {
+        binding.button.setOnClickListener {
+            
+        }
+    }
+
+    private fun readTemperature() = try {
+        binding.inputTemperature.toString().toDouble()
+    } catch (e: NumberFormatException) {
+        throw NumberFormatException("Conversion Error.")
+    }
+
+    private fun readSpinnerValue(spinner: Spinner) = spinner.selectedItem.toString()
+
+
 }
