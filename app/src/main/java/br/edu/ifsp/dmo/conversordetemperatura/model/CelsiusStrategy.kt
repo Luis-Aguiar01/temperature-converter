c0467d80da787bf4c3a8af0811f831f674e82947
Luis Henrique Aguiar <luishenrique.aguiar@outlook.com>
2024-11-17 08:24:45 -0300

    Transformando os objetos strategys em singletons

--- app/src/main/java/br/edu/ifsp/dmo/conversordetemperatura/model/CelsiusStrategy.kt
+++ app/src/main/java/br/edu/ifsp/dmo/conversordetemperatura/model/CelsiusStrategy.kt
@@ -1,6 +1,6 @@
 package br.edu.ifsp.dmo.conversordetemperatura.model
 
-class CelsiusStrategy : TemperatureConverter {
+object CelsiusStrategy : TemperatureConverter {
     override fun converter(temperature: Double) = temperature - 273.15
     override fun getScale() = "ºC"
 }
--- app/src/main/java/br/edu/ifsp/dmo/conversordetemperatura/model/FahrenheitStrategy.kt
+++ app/src/main/java/br/edu/ifsp/dmo/conversordetemperatura/model/FahrenheitStrategy.kt
@@ -1,6 +1,6 @@
 package br.edu.ifsp.dmo.conversordetemperatura.model
 
-class FahrenheitStrategy : TemperatureConverter {
+object FahrenheitStrategy : TemperatureConverter {
     override fun converter(temperature: Double) = (temperature - 273.15) * 1.8 + 32
     override fun getScale() = "ºF"
 }
